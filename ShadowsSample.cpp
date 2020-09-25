28b34cdcee8f6b39202321a389c93e725fb95cfd
jiankors <jiankors@gmail.com>
2020-09-25 10:32:24 +0800

    test

--- ShadowsSample.cpp
+++ ShadowsSample.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+    float a = 0.0f;
+
+    if(a == 0.0f)
+    {
+        cout<<"equal";
+    }
+
+    if(a = 0.0f)
+    {
+        cout<<"bad equal";
+    }
+
     return 0;
 }
